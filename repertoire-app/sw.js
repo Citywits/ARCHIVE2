50f67e9b4a677487125417dda49a035784d60e76
Citywits <citywitsofficial@gmail.com>
2026-08-09 21:42:59 -0700

    Refresh cache for resource finder

--- repertoire-app/sw.js
+++ repertoire-app/sw.js
@@ -1,6 +1,5 @@
-const CACHE_NAME = 'repertoire-v4';
+const CACHE_NAME = 'repertoire-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
